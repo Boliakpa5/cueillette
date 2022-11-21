8f52cef9b0112ab7de92324769f1b059f73cc793
Francois Paillon <francoispaillon@hotmail.fr>
2022-11-21 16:14:16 +0100

    Added routes and product controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,33 @@
+class ProductsController < ApplicationController
+before_action :set_user
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = @user
+    if @product.save
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @product = Product.find(params[:id])
+    @product.delete
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def product_params
+    params.require(:product).admit(:name, :description, :price, :category)
+  end
+end
