48226a7a616f2e8f89c1c244230ecfae2101666e
Francois Paillon <francoispaillon@hotmail.fr>
2022-11-21 16:47:53 +0100

    Setup Cloudinary

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   CATEGORIES = ["Légume", "Boisson"]
   belongs_to :user
   has_many :orders
+  has_one_attached :photo
 
   validates :title, :price, :category, :user, presence: true
   validates :price, numericality: true
