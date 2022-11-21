2b14055caf7e30d20bb064a21892e956c01d34ce
Francois Paillon <francoispaillon@hotmail.fr>
2022-11-21 15:31:56 +0100

    added seed, corrected users, start the view

---
+++ app/models/order.rb
@@ -0,0 +1,8 @@
+class Order < ApplicationRecord
+  STATUS = ["pending", "reserved", "collected"]
+  belongs_to :user
+  belongs_to :product
+
+  validates :user, :product, :pickup_date, presence: true
+  validates :status, inclusion: { in: STATUS }
+end
