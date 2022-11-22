073cd2e1fd03be2af6bc8656e54f9d3c87d74593
Etienne Klepal <etienne.klepal@gmail.com>
2022-11-22 11:07:24 +0100

    solved some critical issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
-
-  skip_before_action :authenticate_user!
-  before_action :set_user, only: %i[show edit update destroy]
+  # skip_before_action :authenticate_user!
+  before_action :set_user, only: %i[show]
 
   def index
     @users = User.all
   end
 
-  # def show; end
+  def show; end
 
   # def new
   #   @user = User.new
@@ -39,9 +38,9 @@ class UsersController < ApplicationController
 
   # private
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   # def user_params
   #   params.require(:user).permit(:first_name, :last_name, :phone_number, :address)
