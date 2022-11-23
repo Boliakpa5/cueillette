584dabd99cfbad65b2e9da1668abf082f39be205
Francois Paillon <francoispaillon@hotmail.fr>
2022-11-23 15:13:28 +0100

    Many things

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,11 @@ class Order < ApplicationRecord
   belongs_to :product
 
   validates :user, :product, :pickup_date, presence: true
+  validate :pickup_date_cannot_be_in_the_past
+
+  def pickup_date_cannot_be_in_the_past
+    if pickup_date.present? && pickup_date < Date.today
+      errors.add(:pickup_date, "not valid, you are a petit malin, but we pensed à tout")
+    end
+  end
 end
